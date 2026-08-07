afed6e40e3592ded8dd38376e718863afb4c9c84
parkura <parkura@users.noreply.github.com>
2026-08-07 11:55:22 -0400

    bump feature field to v17; protect helm overrides in appset ignores

--- src/server.mjs
+++ src/server.mjs
@@ -30,7 +30,7 @@ const server = http.createServer(async (req, res) => {
 	}
 
 	res.setHeader("content-type", "application/json");
-	res.end(JSON.stringify({ service: name, version, feature: "v16-release" }));
+	res.end(JSON.stringify({ service: name, version, feature: "v17-supersede" }));
 });
 
 server.listen(3000, () => console.log(`${name} ${version} listening on 3000`));
